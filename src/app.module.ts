b59359d056c4a9e92f13150365da9825868d65ff
IAnjy <ianjymartial@gmail.com>
2023-04-16 22:37:48 +0300

    config database, adding prisma, make model, setting prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { ProfesseurModule } from './professeur/professeur.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, ProfesseurModule, PrismaModule],
 })
 export class AppModule {}
